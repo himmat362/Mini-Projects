7d482f0617c89ddecfd732b0a08c8ee82ae59f30
himmat362 <107528345+himmat362@users.noreply.github.com>
2022-07-01 09:56:32 +0530

    Update Subset using Random_Final_Perfect_Solution.py

--- Subset using Random_Final_Perfect_Solution.py
+++ Subset using Random_Final_Perfect_Solution.py
@@ -1,18 +1,23 @@
-#subset problem
+#Subset problem where the sum of the subset is zero
 import random as r
 
 
 def random(subset,_final_list_):
-    new_list=r.sample(subset, k=5)
+    #chosing a new subset randomly from the given set 
+    new_list=r.sample(subset, k=range(3,6)
+    #sorting the new_list
     new_list=sorted(new_list)
     if check_sum(new_list)==True:
+        #checking whether the subset already exists in _final_list_
         if new_list in _final_list_:
             return random(subset, _final_list_)
         else:
+            #appending _final_list_ with the new_list
             _final_list_.append(new_list)
             return random(subset, _final_list_)
 
 
+#to check the sum of the selected subset
 def check_sum(_new_list):
     if sum(_new_list)==0:
         return True
@@ -23,6 +28,7 @@ def check_sum(_new_list):
 subset=[1,2,7,5,-9,-1,3,-2,-4]
 final_list=[]
 List=[]
+#number of iterations
 for i in range(10000):
     List=random(subset,final_list)
 print(final_list)
